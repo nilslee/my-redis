3402389bff47a7ad0923e2e179ef0dd955b39506
Aaron Lee <aaronlee232@gmail.com>
2026-03-10 21:09:18 -0500

    Fix spacing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.channels.SocketChannel;
 
 public class Main {
   public static void main(String[] args){
@@ -9,31 +10,30 @@ public class Main {
     System.out.println("Logs from your program will appear here!");
 
     //  Uncomment the code below to pass the first stage
-       ServerSocket serverSocket = null;
-       Socket clientSocket = null;
-       int port = 6379;
+     ServerSocket serverSocket = null;
+     Socket clientSocket = null;
+     int port = 6379;
+     try {
+       serverSocket = new ServerSocket(port);
+       // Since the tester restarts your program quite often, setting SO_REUSEADDR
+       // ensures that we don't run into 'Address already in use' errors
+       serverSocket.setReuseAddress(true);
+       // Wait for connection from client. (Blocking)
+       clientSocket = serverSocket.accept();
+     } catch (IOException e) {
+       System.out.println("IOException: " + e.getMessage());
+     } finally {
        try {
-         serverSocket = new ServerSocket(port);
-         // Since the tester restarts your program quite often, setting SO_REUSEADDR
-         // ensures that we don't run into 'Address already in use' errors
-         serverSocket.setReuseAddress(true);
-         // Wait for connection from client.
-         clientSocket = serverSocket.accept();
+         if (clientSocket != null) {
+           OutputStream outputStream = clientSocket.getOutputStream();
+           outputStream.write("+PONG\r\n".getBytes());
+
+           // Finish by closing socket connection
+           clientSocket.close();
+         }
        } catch (IOException e) {
          System.out.println("IOException: " + e.getMessage());
-       } finally {
-         try {
-           if (clientSocket != null) {
-             // Send Ping
-             OutputStream outputStream = clientSocket.getOutputStream();
-             outputStream.write("+PONG\r\n".getBytes());
-
-             // Finish by closing socket connection
-             clientSocket.close();
-           }
-         } catch (IOException e) {
-           System.out.println("IOException: " + e.getMessage());
-         }
        }
+     }
   }
 }
